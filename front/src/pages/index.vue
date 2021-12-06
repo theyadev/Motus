ccca1b785d659f22ed157391531f5d9fe453145e
theyadev <gtheya@gmail.com>
2021-12-06 11:33:20 +0100

    little change

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-    import Grille from '../components/Grille.vue';
+import Grille from "../components/Grille.vue";
 </script>
 
 <template>
+  <div class="flex justify-around h-screen bg-red-600">
     <Grille :id="1235612" player="Dark" />
+  </div>
 </template>
