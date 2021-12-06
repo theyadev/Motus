7c0aea514778f79d16a38a167b36cf15f4ddbf18
theyadev <gtheya@gmail.com>
2021-12-06 11:33:31 +0100

    added startGame and onStart

--- front/src/stores/socket.ts
+++ front/src/stores/socket.ts
@@ -3,8 +3,20 @@ import { reactive } from "@vue/reactivity";
 
 const socket = reactive(io("http://localhost:8000", {}));
 
+type startCallback = (word: string) => void;
+
 export default function useSocket() {
+  function startGame() {
+    socket.emit("START GAME")
+  }
+
+  function onStart(callback: startCallback) {
+    socket.on("START", (word: string) => {
+      callback(word)
+  })
+  }
+
   return {
-    socket,
+    socket, startGame, onStart
   };
 }
