56b3d4e4c915bcf106954bc26d14e1f0d7bb1daa
Fassi Théo <gtheya@gmail.com>
2021-12-13 15:26:01 +0100

    added gridId and socketId

--- types/Player.ts
+++ types/Player.ts
@@ -1,9 +1,12 @@
 export default class Player {
     username: string;
+    socketId: string;
     host: boolean;
+    gridId?: number; 
   
-    constructor(username: string, host?: boolean) {
+    constructor(username: string, socketId: string, host?: boolean) {
       this.username = username;
       this.host = host || false;
+      this.socketId = socketId
     }
   }
