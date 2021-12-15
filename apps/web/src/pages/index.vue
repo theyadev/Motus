010a7186f1220deac2c95b5fcdb53b6599776007
Fassi Théo <gtheya@gmail.com>
2021-12-15 10:29:45 +0100

    removed grid

--- apps/web/src/pages/index.vue
+++ apps/web/src/pages/index.vue
@@ -2,14 +2,13 @@
 import { ref } from "vue";
 import useSocket from "../stores/socket"
 import { useRouter } from "vue-router";
-import Grille from "../components/Grille.vue";
 
 const router = useRouter()
 
 let username = ref<string>("")
 const { createGame } = useSocket()
 
-async function create(){
+async function create() {
   const id = await createGame(username.value)
 
   router.push("/game/" + id.split("game:")[1])
@@ -18,15 +17,11 @@ async function create(){
 </script>
 
 <template>
-  <!-- <div class="flex justify-around h-screen bg-red-600">
-    <Grille :roomId="1235612" gridId="Dark" />
-  </div> -->
   <div class="flex h-screen justify-center items-center">
-  <div class="flex flex-col w-1/2 space-y-2 py-16 px-16 border border-fuchsia-500 items-center">
-    <div class="text-3xl">Motus !</div>
-    <input v-model="username" class="border border-fuchsia-900"  placeholder="Pseudonyme" />
-    <button class="border border-fuchsia-900" @click="create">Créer</button>
+    <div class="flex flex-col w-1/2 space-y-2 py-16 px-16 border border-fuchsia-500 items-center">
+      <div class="text-3xl">Motus !</div>
+      <input v-model="username" class="border border-fuchsia-900" placeholder="Pseudonyme" />
+      <button class="border border-fuchsia-900" @click="create">Créer</button>
+    </div>
   </div>
-  </div>
-  
 </template>
